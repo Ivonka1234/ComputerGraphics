eb8ef064870ed3ea83b6efe49df6646cb3c6edef
Ivona Sareska <is30672@seeu.edu.mk>
2025-11-19 11:58:20 +0100

    Adding class practicles for textures

---
+++ week10Textures/main.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three'
+
+const scene = new THREE.Scene();
+scene.background=new THREE.Color(0x202020);
+const camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.z=7;
+const renderer=new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+const textureLoader=new THREE.TextureLoader();
+const texture = textureLoader.load(
+    'textures/textures/Stylized_Stone_Floor_010_basecolor.png'
+);
+
+texture.wrapS= THREE.RepeatWrapping;
+texture.wrapT= THREE.RepeatWrapping;
+texture.repeat.set(4,2);
+
+const material=new THREE.MeshBasicMaterial({map:texture});
+const cube=new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    material
+);
+scene.add(cube);
+
+function animate(){
+    requestAnimationFrame(animate);
+    cube.rotation.x+=0.01;
+    renderer.render(scene,camera);
+}
+animate();
